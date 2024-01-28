7a84d9ceb9a9eaa89054338c77eece3deafcb83d
AVANI-GAJJAr <avanigajjar20@gnu.ac.in>
2024-01-28 23:49:29 +0530

    login page and dashboard ui added

--- Fake_Profile_Detection/settings.py
+++ Fake_Profile_Detection/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hu#6hf6tt2y!pdrin&z(-g-ute&t7-ptk^!ns=gt_hz5r%^-a=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Fake_Profile_Detection.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#manually added codes
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,"static")
+]
--- Insightio/views.py
+++ Insightio/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.urls import reverse
+from django.template import loader
+from django.http import HttpResponse,HttpResponseRedirect
 # Create your views here.
+def index(request):
+    return render(request,'login.html')
+def dashboard(request):
+    return render(request,'dash_board.html')
